64888c7d681d6e66b19b24884b74f59aa154b162
Renan Melo <Tecnologia@conjel.com.br>
2026-02-18 14:12:49 -0300

    ajustes

--- DadosSuperlogica/settings.py
+++ DadosSuperlogica/settings.py
@@ -50,19 +50,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DadosSuperlogica.wsgi.application'
 
-if os.environ.get('DATABASE_URL'):
-    DATABASES = { 
-        'defaltu': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age = 600,
-            ssl_require=True,
-            )
-        }
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 
