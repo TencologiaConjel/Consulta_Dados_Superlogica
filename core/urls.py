c893f8cf70ac5e359d697410284992d7c45eec91
Renan Melo <Tecnologia@conjel.com.br>
2026-02-19 08:11:11 -0300

    ajustes

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
        path('dashboard/', DashboardView.as_view(), name='dashboard'),
        path("importar-xlsx/", ImportarXlsxReceitasView.as_view(), name="importar_xlsx_receitas"),
        path("importar-xlsx-despesas/",ImportarXlsxDespesasView.as_view(), name="importar_xlsx_despesas"),
-       path("bootstrap-admin/", bootstrap_superuser, name="bootstrap_admin"),
    ]
